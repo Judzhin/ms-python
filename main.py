da03fcf25224427843d70c983f39af731222ee93
Judzhin Miles <info@msbios.com>
2024-07-10 16:00:37 +0300

    Add Comment

--- main.py
+++ main.py
@@ -6,3 +6,10 @@
 # firstName = input("Enter first name: ")
 # lastName = input("Enter last name: ")
 # print(f"Hi! {firstName} {lastName}. Nice to meуt you!")
+
+## Lesson 4
+# num_1 = input("Enter first number: ")
+num_1 = int (input("Enter first number: "))
+num_2 = input("Enter second number: ")
+result = num_1 + int(num_2);
+print (f"Result ({num_1} + {num_2}) is", result)
