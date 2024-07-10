f0aa6a9c76ea7a73cfa7b3406df78cd29ef249a6
Judzhin Miles <info@msbios.com>
2024-07-10 15:47:14 +0300

    CLI

---
+++ main.py
@@ -0,0 +1,8 @@
+# print ("Hello World!")
+# print (3 + 2)
+# input_string_var = input("Введите данные: ")
+# print (input_string_var)
+
+# firstName = input("Enter your first name: ")
+# lastName = input("Enter yout last name: ")
+# print(f"Hi! {firstName} {lastName}. Nice to meуt you!")
