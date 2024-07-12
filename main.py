ee686f6e66d7acc00181ac600a2229b26fc182fc
Judzhin Miles <info@msbios.com>
2024-07-12 15:51:04 +0300

    add list fn

--- main.py
+++ main.py
@@ -31,16 +31,39 @@
 
 ## Lesson 6
 
-i = 0 
-while i < 10:
-    print(i)
-    i +=1
+# i = 0 
+# while i < 10:
+#     print(i)
+#     i +=1
 
-for j in "Hello world!".upper():
-    if "h" == j.lower():
-        continue
-    print(j*2, end="")
-    if "a" == j.lower():
-        break
-else:
-    print("Letter `A` not found in the text")
+# for j in "Hello world!".upper():
+#     if "h" == j.lower():
+#         continue
+#     print(j*2, end="")
+#     if "a" == j.lower():
+#         break
+# else:
+#     print("Letter `A` not found in the text")
+
+l = []
+numCollection = [1, 23, 43, 5, ['L', 'i', 's', 't'], 7, 87]
+print(numCollection)
+
+a = [a + b for a in 'list' if a != 's' for b in 'soup' if b != 'u']
+print(a)
+
+l.append(1)
+l.append(55)
+b = [24, 65]
+l.extend(b)
+print(f"Append two elements {l}")
+l.insert(2, 33)
+print(f"Insert element by index {l}")
+l.append(88)
+print(l)
+l.remove(88)
+print(f"Remove by element {l}")
+l.pop(1)
+print(f"Remove by index {l}")
+print(f"Show index by element {l.index(24)}")
+print(f"Count by element {l.count(24)}")
